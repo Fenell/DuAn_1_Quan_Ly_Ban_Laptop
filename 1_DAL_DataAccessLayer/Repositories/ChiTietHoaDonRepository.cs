5d843f67fa8593a0e4fa384cfb2f9b71d294f150
Hieuhieuhieu1012 <117344638+Hieuhieuhieu1012@users.noreply.github.com>
2023-03-20 21:43:52 +0700

    Hieu commit

--- 1_DAL_DataAccessLayer/Repositories/ChiTietHoaDonRepository.cs
+++ 1_DAL_DataAccessLayer/Repositories/ChiTietHoaDonRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _1_DAL_DataAccessLayer.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace _1_DAL_DataAccessLayer.Repositories
 {
-    internal class ChiTietHoaDonRepository
+    public class ChiTietHoaDonRepository
     {
+        public void Add()
+        {
+
+        }
     }
 }
