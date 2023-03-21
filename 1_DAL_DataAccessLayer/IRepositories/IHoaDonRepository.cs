a613c721db49a34cef0d5577213bfefd1bcc25ed
DESKTOP-51CTS9F\LXMC <lxmchien@gmail.com>
2023-03-21 22:44:11 +0700

    Chiến Update IRepository , Repository: HoaDon,NhanVien,KhachHang,LapTop

---
+++ 1_DAL_DataAccessLayer/IRepositories/IHoaDonRepository.cs
@@ -0,0 +1,16 @@
+﻿using _1_DAL_DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_DAL_DataAccessLayer.IRepositories
+{
+    public interface IHoaDonRepository
+    {
+        bool AddHoaDon(HoaDon hoaDon);
+        HoaDon GetHoaDonById(Guid idHoaDon);
+        List<HoaDon> GetAllHoaDon();
+    }
+}
