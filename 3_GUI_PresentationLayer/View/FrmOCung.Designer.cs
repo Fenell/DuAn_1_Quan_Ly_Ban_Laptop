ad0ca1ca357bc2adf86c41b97d454026f69222f0
TuanDat <maituandat087804@gmail.com>
2023-03-22 15:42:42 +0700

    Update giao diện + db

---
+++ 3_GUI_PresentationLayer/View/FrmOCung.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace _3_GUI_PresentationLayer.View
+{
+    partial class FrmOCung
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel2 = new Panel();
+            textBoxCustom2_02 = new CustomControl.TextBoxCustom2_0();
+            radioButtonCustom2 = new CustomControl.RadioButtonCustom();
+            radioButtonCustom1 = new CustomControl.RadioButtonCustom();
+            iconButton1 = new FontAwesome.Sharp.IconButton();
+            iconButton5 = new FontAwesome.Sharp.IconButton();
+            iconButton3 = new FontAwesome.Sharp.IconButton();
+            iconButton2 = new FontAwesome.Sharp.IconButton();
+            textBoxCustom2_01 = new CustomControl.TextBoxCustom2_0();
+            label3 = new Label();
+            label2 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label4 = new Label();
+            label1 = new Label();
+            panel1 = new Panel();
+            dataGridView1 = new DataGridView();
+            panel2.SuspendLayout();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.White;
+            panel2.Controls.Add(textBoxCustom2_02);
+            panel2.Controls.Add(radioButtonCustom2);
+            panel2.Controls.Add(radioButtonCustom1);
+            panel2.Controls.Add(iconButton1);
+            panel2.Controls.Add(iconButton5);
+            panel2.Controls.Add(iconButton3);
+            panel2.Controls.Add(iconButton2);
+            panel2.Controls.Add(textBoxCustom2_01);
+            panel2.Controls.Add(label3);
+            panel2.Controls.Add(label2);
+            panel2.Controls.Add(label5);
+            panel2.Controls.Add(label6);
+            panel2.Controls.Add(label4);
+            panel2.Controls.Add(label1);
+            panel2.Location = new Point(286, 15);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(250, 237);
+            panel2.TabIndex = 4;
+            // 
+            // textBoxCustom2_02
+            // 
+            textBoxCustom2_02.BorderColor = Color.DarkOrchid;
+            textBoxCustom2_02.BorderFocusColor = Color.HotPink;
+            textBoxCustom2_02.BorderRadius = 10;
+            textBoxCustom2_02.BorderSize = 2;
+            textBoxCustom2_02.Location = new Point(99, 142);
+            textBoxCustom2_02.Multiline = false;
+            textBoxCustom2_02.Name = "textBoxCustom2_02";
+            textBoxCustom2_02.Padding = new Padding(7);
+            textBoxCustom2_02.PasswordChar = false;
+            textBoxCustom2_02.PlaceholderColor = Color.DarkGray;
+            textBoxCustom2_02.PlaceholderText = "";
+            textBoxCustom2_02.Size = new Size(95, 30);
+            textBoxCustom2_02.TabIndex = 21;
+            textBoxCustom2_02.Texts = "";
+            textBoxCustom2_02.UnderlinedStyle = true;
+            // 
+            // radioButtonCustom2
+            // 
+            radioButtonCustom2.AutoSize = true;
+            radioButtonCustom2.CheckedColor = Color.MediumSlateBlue;
+            radioButtonCustom2.Location = new Point(158, 107);
+            radioButtonCustom2.MinimumSize = new Size(0, 21);
+            radioButtonCustom2.Name = "radioButtonCustom2";
+            radioButtonCustom2.Padding = new Padding(10, 0, 0, 0);
+            radioButtonCustom2.Size = new Size(55, 21);
+            radioButtonCustom2.TabIndex = 20;
+            radioButtonCustom2.TabStop = true;
+            radioButtonCustom2.Text = "SSD";
+            radioButtonCustom2.UnCheckedColor = Color.Gray;
+            radioButtonCustom2.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonCustom1
+            // 
+            radioButtonCustom1.AutoSize = true;
+            radioButtonCustom1.CheckedColor = Color.MediumSlateBlue;
+            radioButtonCustom1.Location = new Point(77, 107);
+            radioButtonCustom1.MinimumSize = new Size(0, 21);
+            radioButtonCustom1.Name = "radioButtonCustom1";
+            radioButtonCustom1.Padding = new Padding(10, 0, 0, 0);
+            radioButtonCustom1.Size = new Size(60, 21);
+            radioButtonCustom1.TabIndex = 20;
+            radioButtonCustom1.TabStop = true;
+            radioButtonCustom1.Text = "HDD";
+            radioButtonCustom1.UnCheckedColor = Color.Gray;
+            radioButtonCustom1.UseVisualStyleBackColor = true;
+            // 
+            // iconButton1
+            // 
+            iconButton1.FlatAppearance.BorderSize = 0;
+            iconButton1.FlatStyle = FlatStyle.Flat;
+            iconButton1.IconChar = FontAwesome.Sharp.IconChar.Trash;
+            iconButton1.IconColor = Color.FromArgb(61, 39, 76);
+            iconButton1.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            iconButton1.IconSize = 40;
+            iconButton1.Location = new Point(121, 195);
+            iconButton1.Name = "iconButton1";
+            iconButton1.Size = new Size(53, 37);
+            iconButton1.TabIndex = 19;
+            iconButton1.UseVisualStyleBackColor = true;
+            // 
+            // iconButton5
+            // 
+            iconButton5.FlatAppearance.BorderSize = 0;
+            iconButton5.FlatStyle = FlatStyle.Flat;
+            iconButton5.IconChar = FontAwesome.Sharp.IconChar.Rotate;
+            iconButton5.IconColor = Color.FromArgb(61, 39, 76);
+            iconButton5.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            iconButton5.IconSize = 40;
+            iconButton5.Location = new Point(171, 195);
+            iconButton5.Name = "iconButton5";
+            iconButton5.Size = new Size(53, 37);
+            iconButton5.TabIndex = 19;
+            iconButton5.UseVisualStyleBackColor = true;
+            // 
+            // iconButton3
+            // 
+            iconButton3.FlatAppearance.BorderSize = 0;
+            iconButton3.FlatStyle = FlatStyle.Flat;
+            iconButton3.IconChar = FontAwesome.Sharp.IconChar.Pen;
+            iconButton3.IconColor = Color.FromArgb(61, 39, 76);
+            iconButton3.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            iconButton3.IconSize = 40;
+            iconButton3.Location = new Point(62, 195);
+            iconButton3.Name = "iconButton3";
+            iconButton3.Size = new Size(53, 37);
+            iconButton3.TabIndex = 18;
+            iconButton3.UseVisualStyleBackColor = true;
+            // 
+            // iconButton2
+            // 
+            iconButton2.FlatAppearance.BorderSize = 0;
+            iconButton2.FlatStyle = FlatStyle.Flat;
+            iconButton2.IconChar = FontAwesome.Sharp.IconChar.Add;
+            iconButton2.IconColor = Color.FromArgb(61, 39, 76);
+            iconButton2.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            iconButton2.IconSize = 40;
+            iconButton2.Location = new Point(7, 195);
+            iconButton2.Name = "iconButton2";
+            iconButton2.Size = new Size(53, 37);
+            iconButton2.TabIndex = 17;
+            iconButton2.UseVisualStyleBackColor = true;
+            // 
+            // textBoxCustom2_01
+            // 
+            textBoxCustom2_01.BorderColor = Color.DarkOrchid;
+            textBoxCustom2_01.BorderFocusColor = Color.HotPink;
+            textBoxCustom2_01.BorderRadius = 10;
+            textBoxCustom2_01.BorderSize = 2;
+            textBoxCustom2_01.Location = new Point(13, 68);
+            textBoxCustom2_01.Multiline = false;
+            textBoxCustom2_01.Name = "textBoxCustom2_01";
+            textBoxCustom2_01.Padding = new Padding(7);
+            textBoxCustom2_01.PasswordChar = false;
+            textBoxCustom2_01.PlaceholderColor = Color.DarkGray;
+            textBoxCustom2_01.PlaceholderText = "";
+            textBoxCustom2_01.Size = new Size(221, 30);
+            textBoxCustom2_01.TabIndex = 1;
+            textBoxCustom2_01.Texts = "";
+            textBoxCustom2_01.UnderlinedStyle = true;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.White;
+            label3.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(66, 5);
+            label3.Name = "label3";
+            label3.Size = new Size(34, 17);
+            label3.TabIndex = 0;
+            label3.Text = "1234";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.White;
+            label2.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(13, 5);
+            label2.Name = "label2";
+            label2.Size = new Size(27, 17);
+            label2.TabIndex = 0;
+            label2.Text = "Mã";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(13, 149);
+            label5.Name = "label5";
+            label5.Size = new Size(80, 17);
+            label5.TabIndex = 0;
+            label5.Text = "Dung lượng";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.Location = new Point(203, 152);
+            label6.Name = "label6";
+            label6.Size = new Size(25, 17);
+            label6.TabIndex = 0;
+            label6.Text = "GB";
+            label6.Click += label4_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(13, 111);
+            label4.Name = "label4";
+            label4.Size = new Size(32, 17);
+            label4.TabIndex = 0;
+            label4.Text = "Loại";
+            label4.Click += label4_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(13, 33);
+            label1.Name = "label1";
+            label1.Size = new Size(75, 17);
+            label1.TabIndex = 0;
+            label1.Text = "Tên ổ cứng";
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.White;
+            panel1.Controls.Add(dataGridView1);
+            panel1.Location = new Point(12, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(265, 240);
+            panel1.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(6, 3);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(259, 232);
+            dataGridView1.TabIndex = 0;
+            // 
+            // FrmOCung
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(548, 259);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "FrmOCung";
+            Text = "FrmOCung";
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private Panel panel2;
+        private CustomControl.RadioButtonCustom radioButtonCustom2;
+        private CustomControl.RadioButtonCustom radioButtonCustom1;
+        private FontAwesome.Sharp.IconButton iconButton1;
+        private FontAwesome.Sharp.IconButton iconButton5;
+        private FontAwesome.Sharp.IconButton iconButton3;
+        private FontAwesome.Sharp.IconButton iconButton2;
+        private CustomControl.TextBoxCustom2_0 textBoxCustom2_01;
+        private Label label3;
+        private Label label2;
+        private Label label4;
+        private Label label1;
+        private Panel panel1;
+        private DataGridView dataGridView1;
+        private CustomControl.TextBoxCustom2_0 textBoxCustom2_02;
+        private Label label5;
+        private Label label6;
+    }
+}
