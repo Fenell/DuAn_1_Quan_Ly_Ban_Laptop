89ebc98fd4590dd4748918f751e19bf1f91df0d2
Hieuhieuhieu1012 <117344638+Hieuhieuhieu1012@users.noreply.github.com>
2023-03-23 15:40:00 +0700

    Hieu 23.3

--- 1_DAL_DataAccessLayer/Repositories/ChiTietLapTopRepository.cs
+++ 1_DAL_DataAccessLayer/Repositories/ChiTietLapTopRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using _1_DAL_DataAccessLayer.IRepositories;
+using _1_DAL_DataAccessLayer.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,26 @@ using System.Threading.Tasks;
 
 namespace _1_DAL_DataAccessLayer.Repositories
 {
-    public class ChiTietLapTopRepository
+    public class ChiTietLapTopRepository : ILapTopRepository
     {
+        public bool AddLapTop(Laptop lapTop)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Laptop> GetAllLapTop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Laptop GetLapTopById(Guid idLapTop)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateLapTop(Guid idLapTop, Laptop lapTop)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
