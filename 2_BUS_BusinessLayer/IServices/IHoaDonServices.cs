e3e1a368e45fc23ebdbe42ab7ced8d349cfd844d
Hieuhieuhieu1012 <117344638+Hieuhieuhieu1012@users.noreply.github.com>
2023-03-25 15:50:39 +0700

    frmhoaDon

--- 2_BUS_BusinessLayer/IServices/IHoaDonServices.cs
+++ 2_BUS_BusinessLayer/IServices/IHoaDonServices.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using _1_DAL_DataAccessLayer.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,10 @@ using System.Threading.Tasks;
 
 namespace _2_BUS_BusinessLayer.IServices
 {
-    internal interface IHoaDonServices
+    public interface IHoaDonServices
     {
+        bool Add(HoaDon hoaDon);
+        bool Sua(HoaDon hoaDon);
+        List<HoaDon> GetAll();
     }
 }
--- 3_GUI_PresentationLayer/View/FrmHoaDon.cs
+++ 3_GUI_PresentationLayer/View/FrmHoaDon.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using _1_DAL_DataAccessLayer.Models;
+using _2_BUS_BusinessLayer.IServices;
+using _2_BUS_BusinessLayer.Services;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +15,32 @@ namespace _3_GUI_PresentationLayer.View
 {
     public partial class FrmHoaDon : Form
     {
+        IHoaDonServices _Bus_hoaDon;
+        List<HoaDon> _lst_hoaDon;
         public FrmHoaDon()
         {
             InitializeComponent();
+            ShowHĐ();
+        }
+        public void ShowHĐ()
+        {
+            
+            dgv_hoaDon.ColumnCount = 10;
+            dgv_hoaDon.Columns[0].Name = " ID";
+            dgv_hoaDon.Columns[1].Name = " IdNhanVien";
+            dgv_hoaDon.Columns[2].Name = " IdKhachHang";
+            dgv_hoaDon.Columns[3].Name = " Ma";
+            dgv_hoaDon.Columns[4].Name = " HinhThucTT";
+            dgv_hoaDon.Columns[5].Name = " NgayTao";
+            dgv_hoaDon.Columns[6].Name = " NgayThanhToan";
+            dgv_hoaDon.Columns[7].Name = " DiaChiNhanHang";
+            dgv_hoaDon.Columns[8].Name = " Ghi chú";
+            dgv_hoaDon.Columns[9].Name = " TongTien";
+            dgv_hoaDon.Rows.Clear();
+            foreach (var item in _Bus_hoaDon.GetAll())
+            {
+                dgv_hoaDon.Rows.Add(item.Id, item.IdNhanVien, item.IdKhachHang, item.Ma, item.HinhThucTT, item.NgayTao, item.NgayThanhToan, item.DiaChiNhanHang, item.GhiChu, item.TongTien);
+            }
         }
 
        
