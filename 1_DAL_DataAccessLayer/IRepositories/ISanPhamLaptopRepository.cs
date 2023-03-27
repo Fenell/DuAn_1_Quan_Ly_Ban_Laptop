2019ff233e2352404010262d474603bc56417766
DESKTOP-51CTS9F\LXMC <lxmchien@gmail.com>
2023-03-27 22:15:50 +0700

    Commit

--- 1_DAL_DataAccessLayer/IRepositories/ISanPhamLaptopRepository.cs
+++ 1_DAL_DataAccessLayer/IRepositories/ISanPhamLaptopRepository.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 namespace _1_DAL_DataAccessLayer.IRepositories
 {
     public interface ISanPhamLaptopRepository
+>>>>>>>> Dat:1_DAL_DataAccessLayer/IRepositories/ISanPhamLapTopRepository.cs
     {
         bool AddLapTop(Laptop lapTop);
         bool UpdateLapTop(Laptop lapTop);
