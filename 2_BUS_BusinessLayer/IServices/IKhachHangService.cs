97b1ad533aeb25488e2b34a3e8db081838c4b914
TuanDat <maituandat087804@gmail.com>
2023-03-29 22:50:43 +0700

    Thêm KhachHangService

---
+++ 2_BUS_BusinessLayer/IServices/IKhachHangService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _2_BUS_BusinessLayer.ViewModel;
+
+namespace _2_BUS_BusinessLayer.IServices
+{
+    public interface IKhachHangService
+    {
+        public List<KhachHangView> GetLstKhachHang();
+    }
+}
---
+++ 2_BUS_BusinessLayer/Services/KhachHangService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using _1_DAL_DataAccessLayer.IRepositories;
+using _1_DAL_DataAccessLayer.Repositories;
+using _2_BUS_BusinessLayer.IServices;
+using _2_BUS_BusinessLayer.ViewModel;
+
+namespace _2_BUS_BusinessLayer.Services
+{
+    public class KhachHangService:IKhachHangService
+    {
+        private IKhachHangRepository _khachHangRepository;
+
+        public KhachHangService()
+        {
+            _khachHangRepository = new KhachHangRepository();
+        }
+        public List<KhachHangView> GetLstKhachHang()
+        {
+            var lst = (from a in _khachHangRepository.GetAllKhachHang()
+                select new KhachHangView()
+                {
+                    Id = a.Id,
+                    Ma = a.Ma,
+                    Hoten = a.Hoten,
+                    DiaChi = a.DiaChi,
+                    GioiTinh = a.GioiTinh,
+                    SoDienThoai = a.SoDienThoai,
+                }).ToList();
+            return lst;
+        }
+    }
+}
