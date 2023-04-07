b79ae407eeccae993934413f9b0642c78418d555
TuanDat <maituandat087804@gmail.com>
2023-04-07 15:08:11 +0700

    update bán hàng 99

---
+++ 1_DAL_DataAccessLayer/Migrations/20230406142921_uppdateee.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace _1_DAL_DataAccessLayer.Migrations
+{
+    public partial class uppdateee : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "GiamGia",
+                table: "ChiTietHoaDon",
+                type: "decimal(18,0)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "GiamGia",
+                table: "ChiTietHoaDon");
+        }
+    }
+}
